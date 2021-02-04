4b87f1c97c1f8035c71b4a14cd0d0cb234beed19
portaali <ville.perkkio@businessoulu.com>
2021-02-04 14:54:30 +0200

    Adding version info for chatbox

--- src/Koha/Plugin/Com/Honkaportaali/AspaTukiChatPlugin.pm
+++ src/Koha/Plugin/Com/Honkaportaali/AspaTukiChatPlugin.pm
@@ -28,9 +28,9 @@ use CGI::Carp qw/fatalsToBrowser/;
 use strict;
 
 ## Here we set our plugin version
-our $VERSION = "1.2.5";
+our $VERSION = "1.2.7";
 ## Date updated
-our $DATE_UPDATED = "2021-02-03";
+our $DATE_UPDATED = "2021-02-04";
 
 ## Here is our metadata, some keys are required, some are optional
 our $metadata = {
