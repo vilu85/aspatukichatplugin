01259c7708dc1ab9469557fc6ede2feb7634f14b
vilu <vilu85@users.noreply.github.com>
2021-03-14 14:20:30 +0200

    Resizing chat box and fine tuning css

--- src/Koha/Plugin/Com/Honkaportaali/AspaTukiChatPlugin.pm
+++ src/Koha/Plugin/Com/Honkaportaali/AspaTukiChatPlugin.pm
@@ -28,9 +28,9 @@ use CGI::Carp qw/fatalsToBrowser/;
 use strict;
 
 ## Here we set our plugin version
-our $VERSION = "1.2.12";
+our $VERSION = "1.2.13";
 ## Date updated
-our $DATE_UPDATED = "2021-02-24";
+our $DATE_UPDATED = "2021-03-14";
 
 ## Here is our metadata, some keys are required, some are optional
 our $metadata = {
