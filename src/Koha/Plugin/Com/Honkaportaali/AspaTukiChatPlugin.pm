baa320e5abbfb9992da170762bb8d003890c7f5a
portaali <ville.perkkio@businessoulu.com>
2021-03-17 09:19:08 +0200

    Fixed an issue with hovering

--- src/Koha/Plugin/Com/Honkaportaali/AspaTukiChatPlugin.pm
+++ src/Koha/Plugin/Com/Honkaportaali/AspaTukiChatPlugin.pm
@@ -28,9 +28,9 @@ use CGI::Carp qw/fatalsToBrowser/;
 use strict;
 
 ## Here we set our plugin version
-our $VERSION = "1.3.1";
+our $VERSION = "1.3.2";
 ## Date updated
-our $DATE_UPDATED = "2021-03-16";
+our $DATE_UPDATED = "2021-03-17";
 
 ## Here is our metadata, some keys are required, some are optional
 our $metadata = {
